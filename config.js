72bc2844cc439cc79310a4c4f7113b1245020e77
gabriel09hyu <160981913+gabriel09hyu@users.noreply.github.com>
2025-09-18 20:19:05 -0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-export const TMDB_API_KEY = 'd62492ee51bbba141d7a8f0c7daa6e30';
-
-// IMPORTANTE: A chave do Gemini NUNCA deve ser exposta no lado do cliente.
-// O ideal é usar uma função de servidor (serverless function) para fazer a chamada.
-export const GEMINI_API_KEY = "AIzaSyBn8mLJXIaZgUX_NyOIkYqRXY9uGbL9LZI";
