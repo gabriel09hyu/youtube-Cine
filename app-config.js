143faa1ca071c46d1b176df34ebd43621c0574df
gabriel09hyu <gabrielhtml900@gmail.com>
2025-09-18 23:38:15 -0300

    FIX ERROR 4

--- app-config.js
+++ app-config.js
@@ -1,2 +1,5 @@
  // Este arquivo é um placeholder.
  // O workflow do GitHub Actions irá sobrescrevê-lo com as chaves de API durante o deploy.
+
+ export const GEMINI_API_KEY = '${{ secrets.VITE_GEMINI_API_KEY }}';
+ export const TMDB_API_KEY = '${{ secrets.VITE_TMDB_API_KEY }}';
